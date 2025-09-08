2ef2e746b5eb44bfc650bccf251d5c201fa6b987
jfraser77 <joefraser7@gmail.com>
2025-09-08 14:29:07 -0500

    Images and Font optimized

--- app/ui/components/nav-links.tsx
+++ app/ui/components/nav-links.tsx
@@ -10,6 +10,7 @@ import {
 // Depending on the size of the application, this would be stored in a database.
 import { usePathname } from "next/navigation";
 import Link from "next/link";
+
 import clsx from "clsx";
 
 const links = [
@@ -23,7 +24,7 @@ const links = [
   { name: "Contact", href: "/blog/contact", icon: EnvelopeIcon },
 ];
 
-export default async function NavLinks() {
+export default function NavLinks() {
   const pathname = usePathname();
   return (
     <>
@@ -33,9 +34,13 @@ export default async function NavLinks() {
           <Link
             key={link.name}
             href={link.href}
-            className="'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-purple-100 md:flex-none md:justify-start md:p-2 md:px-3'"
+            className={clsx(
+              "flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-purple-100 md:flex-none md:justify-start md:p-2 md:px-3",
+              { "bg-purple-100": pathname === link.href }
+            )}
           >
-            {/* <LinkIcon className="w-6" /> */}
+            {/* Icon rendering removed since LinkIcon was unused */}
+            <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
           </Link>
         );
