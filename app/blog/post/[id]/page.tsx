37fcbdf83943c3e886530bddd066692a4854b806
jfraser77 <joefraser7@gmail.com>
2025-09-09 15:14:13 -0500

    updated Post page

--- app/blog/post/[id]/page.tsx
+++ app/blog/post/[id]/page.tsx
@@ -1,8 +1,18 @@
 import { posts } from "@/app/lib/placeholder-data";
 import Post from "@/app/ui/components/posts/Post";
 
-export default function Page({ params }: { params: { id: string } }) {
-  const post = posts.find((post) => post.id === params.id);
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+  const post = posts.find((post) => post.id === id);
+
+  if (!post) {
+    return <div>Post not found</div>;
+  }
+
   return (
     <>
       <h1>Post</h1>
