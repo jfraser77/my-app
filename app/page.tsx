f8f8efc0bd8be227ec2d7d6ffec1a28e8fd230bb
jfraser77 <joefraser7@gmail.com>
2025-09-09 23:16:55 -0500

    change style to green

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,9 @@ export default function Home() {
   return (
     <main className="flex items-center justify-center min-h-screen bg-gray-50">
       <div className="container mx-auto p-4 relative">
-        <div className="bg-white border-2 border-purple-100 rounded-lg p-6 grid grid-cols-1 md:grid-cols-2 gap-4">
+        <div className="bg-white border-2 border-emerald-500 rounded-lg p-6 grid grid-cols-1 md:grid-cols-2 gap-4">
           <div className="flex flex-col justify-center">
-            <h1 className={`text-4xlfont-bold mb-4 ${styles.text_wisteria}`}>
+            <h1 className={`text-4xlfont-bold mb-4 ${styles.text_green}`}>
               Welcome
             </h1>
             <p className="text-lg text-gray-700 mb-4">
@@ -17,7 +17,7 @@ export default function Home() {
             </p>
             <a
               href="/blog/posts"
-              className={`outline outline-1 outline-offset-2 border-red-700 text-red-700 hover:text-white py-2 px-4 rounded hover:bg-red-800 md:w-auto ${styles.fit_content}`}
+              className={`outline outline-1 outline-offset-2 border-emerald-300 text-emerald-300 hover:text-white py-2 px-4 rounded hover:bg-emerald-300 md:w-auto ${styles.fit_content}`}
             >
               Go to Blog
             </a>
@@ -42,7 +42,7 @@ export default function Home() {
         </div>
       </div>
       <div
-        className={`hidden md:block absolute top-0 right-0 bottom-0 left-2/3 z-0 ${styles.bg_wisteria}`}
+        className={`hidden md:block absolute top-0 right-0 bottom-0 left-2/3 z-0 ${styles.bg_green}`}
       ></div>
     </main>
   );
